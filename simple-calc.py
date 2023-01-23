009c57a677c16368740a737a188d73ac8e1c54c8
C0D3R-1S-COOL <96707622+C0D3R-1S-COOL@users.noreply.github.com>
2023-01-23 17:22:03 -0600

    Different form of input.

--- simple-calc.py
+++ simple-calc.py
@@ -1,3 +1,20 @@
-t = input("Enter an equation here: ")
+#TODO:
+#Finish TODO
 
-print(eval(t))
+def arithmetic(operation, number1, number2):
+  if operation == "M":
+    return number1 * number2
+  elif operation == "D":
+    return number1 / number2
+  elif operation == "A":
+    return number1 + number2
+  elif operation == "S":
+    return number1 - number2
+  else:
+    raise Exception("Sorry, you enetered an invalid value or operation.")
+
+operationInput = input("Enter an operation (M - Multiplication, D - Division, A - Add, S - Subtract): ")
+Number1 = float(input("Enter number 1: "))
+Number2 = float(input("Enter number 2: "))
+answer = arithmetic(operationInput, Number1, Number2)
+print()
