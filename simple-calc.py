faa446f530726e09334ea6b2ad24635c17cd65b7
C0D3R-1S-COOL <96707622+C0D3R-1S-COOL@users.noreply.github.com>
2023-01-23 17:58:09 -0600

    Updated TODO and fixed "print" statement

--- simple-calc.py
+++ simple-calc.py
@@ -1,5 +1,6 @@
 #TODO:
-#Finish TODO
+#Add support for more numbers, operations, and using input from the last answer. (e.g. M 2 5 [10] Y D 6)
+#Add better way to input equations
 
 def arithmetic(operation, number1, number2):
   if operation == "M":
@@ -13,8 +14,8 @@ def arithmetic(operation, number1, number2):
   else:
     raise Exception("Sorry, you enetered an invalid value or operation.")
 
-operationInput = input("Enter an operation (M - Multiplication, D - Division, A - Add, S - Subtract): ")
 Number1 = float(input("Enter number 1: "))
+operationInput = input("Enter an operation (M - Multiplication, D - Division, A - Add, S - Subtract): ")
 Number2 = float(input("Enter number 2: "))
 answer = arithmetic(operationInput, Number1, Number2)
-print()
+print(answer)
