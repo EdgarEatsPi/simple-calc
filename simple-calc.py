5a9a12a52d822ae1d9e629779b46c1e924027d70
C0D3R-1S-COOL <edgarsanty2017@gmail.com>
2023-02-19 14:42:40 -0600

    Now allows for using last answer as first input in the next operation

--- simple-calc.py
+++ simple-calc.py
@@ -14,8 +14,21 @@ def arithmetic(operation, number1, number2):
   else:
     raise Exception("Sorry, you enetered an invalid value or operation.")
 
+
 Number1 = float(input("Enter number 1: "))
 operationInput = input("Enter an operation (M - Multiplication, D - Division, A - Add, S - Subtract): ")
 Number2 = float(input("Enter number 2: "))
 answer = arithmetic(operationInput, Number1, Number2)
 print(answer)
+
+
+useLastAnswer = input("Do you want to reuse the last input in a new operation? (Y - Yes, N - No): ")
+
+if useLastAnswer == Y:
+    lastAnswer = Number1
+    Number1 = lastAnswer
+    operationInput = input("Enter an operation (M - Multiplication, D - Division, A - Add, S - Subtract): ")
+    Number2 = float(input("Enter number 2: "))
+    answer = arithmetic(operationInput, Number1, Number2)
+    print(answer)
+    useLastAnswer = input("Do you want to reuse the last input in a new operation? (Y - Yes, N - No): ")
